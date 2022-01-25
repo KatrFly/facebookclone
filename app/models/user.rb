76c66fbde68fe9774e04618f7f93cb3b6b39b23e
KatrFly <katrien.vliegen@gmail.com>
2022-01-25 20:54:55 +0100

    add friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # skip_before_action :authenticate_user!, only: [:new, :create]
+
+  has_many :posts
+  has_many :likes
+  has_many :comments
+  has_many :received_friend_requests, class_name: 'FriendRequest', foreign_key: 'invitee_id'
+  has_many :sent_friend_requests, class_name: 'FriendRequest', foreign_key: 'inviter_id'
+
+  has_many :friendships
+  has_many :friends, through: :friendships
+
 end
