f990c620e69de5db0e6554e61e0c4bfb9dd6a4cf
KatrFly <katrien.vliegen@gmail.com>
2022-01-25 23:07:39 +0100

    add refences to friendrequest

--- db/migrate/20220125213428_add_references_to_friend_requests.rb
+++ db/migrate/20220125213428_add_references_to_friend_requests.rb
@@ -1,6 +1,6 @@
 class AddReferencesToFriendRequests < ActiveRecord::Migration[6.1]
   def change
-    add_reference :friendrequests, :inviter
-    add_foreign_key :friendrequests, :users
+    add_reference :friend_requests, :inviter_id
+    add_reference :friend_requests, :invitee_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_205302) do
+ActiveRecord::Schema.define(version: 2022_01_25_213428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 2022_01_25_205302) do
   create_table "friend_requests", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "inviter_id_id"
+    t.bigint "invitee_id_id"
+    t.index ["invitee_id_id"], name: "index_friend_requests_on_invitee_id_id"
+    t.index ["inviter_id_id"], name: "index_friend_requests_on_inviter_id_id"
   end
 
   create_table "friendships", force: :cascade do |t|
