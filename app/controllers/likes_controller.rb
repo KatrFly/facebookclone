15e85ce07d835291b35203f3579870e4f23e4e25
KatrFly <katrien.vliegen@gmail.com>
2022-01-26 23:21:11 +0100

    now only possible to like other people's posts that you haven't liked yet

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,10 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(likes_params[:post_id])
-    @like = @post.likes.build(post_id: likes_params[:post_id], user_id: current_user.id)
-    @like.save
+    unless Like.check_if_like_already_exists(@post.likes, current_user.id)
+      @like = @post.likes.build(post_id: likes_params[:post_id], user_id: current_user.id)
+      @like.save 
+    end
     @posts = Post.all
     render "posts/index"
   end
