5cf9d2292e7f8c0628cc851bf5f5991b2624c473
KatrFly <katrien.vliegen@gmail.com>
2022-01-27 18:51:16 +0100

    doesn't show request to 2 users that are already friends (previously it still showed the button to one of them

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,43 @@ class User < ApplicationRecord
 
   # skip_before_action :authenticate_user!, only: [:new, :create]
 
-  has_many :posts
-  has_many :likes
-  has_many :comments
-  has_many :received_friend_requests, class_name: 'FriendRequest', foreign_key: 'invitee_id'
-  has_many :sent_friend_requests, class_name: 'FriendRequest', foreign_key: 'inviter_id'
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :received_friend_requests, class_name: 'FriendRequest', foreign_key: 'invitee_id', dependent: :destroy
+  has_many :sent_friend_requests, class_name: 'FriendRequest', foreign_key: 'inviter_id', dependent: :destroy
 
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
 
+  def all_friends
+    a = []
+    self.friends.each do |friend|
+      a << friend
+    end
+
+    other_friends = Friendship.find_by(friend_id: self.id)
+      unless other_friends == nil
+        begin
+          other_friends.take.each do |friendship|
+            a << friend
+          end
+        rescue
+          a << other_friends.user
+        end
+      end
+    a
+  end
+
   def friends_include_user?(current_user, friend_id)
-    current_user.friends.each do |f|
+    friend_ids = []
+
+    current_user.all_friends.each do |f|
       friend_ids << f.id
     end
 
+    friend_ids
+    friend_id
     return true if friend_ids.include?(friend_id) 
   end
-
 end
